57891dd764c83b97a0259d097071292d0bad8bbc
pin20 <pin200048@gmail.com>
2022-04-04 18:13:01 +0300

    Initial commit

---
+++ src/main/java/com/coursetwo/coursepapertwo/CoursePaperTwoApplication.java
@@ -0,0 +1,13 @@
+package com.coursetwo.coursepapertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaperTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursePaperTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coursetwo/coursepapertwo/CoursePaperTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursetwo.coursepapertwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePaperTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
